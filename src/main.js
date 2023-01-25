e7733f28f57cc8b7764605ba318135538ed47220
szilviabarath <szilvia.barat@gmail.com>
2023-01-25 20:03:06 +0100

    files

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import './assets/main.css'
-import 'chart.js'
-import 'vue-chartjs'
-
-createApp(App).mount('#app')
