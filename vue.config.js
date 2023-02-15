094459f88492a2d3e2ad1b37cadc2b1af998f98a
szilviabarath <56439020+szilviabarath@users.noreply.github.com>
2023-02-15 21:48:53 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-   publicPath: process.env.NODE_ENV === "production" ? "szilviabarath.github.io/dashboard" : "/",
+   publicPath: 'dashboard'
 })
 
 
