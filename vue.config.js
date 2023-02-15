e854e49e6b8ff84bfa3bc0175ea576a8bcc47638
szilviabarath <szilvia.barat@gmail.com>
2023-02-15 22:18:05 +0100

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-   publicPath: 'dashboard'
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/dashboard/'
+  : '/'
 })
 
 
